5ff462e5e2bfe404d516a521ce829a865c4f3756
Tatavarthi Hariteja <tvhhariteja@gmail.com>
2025-07-28 17:27:16 +0530

    Time: 8 ms (5.40%) | Memory: 43.8 MB (5.07%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -3,19 +3,19 @@ class Solution {
         long num=0;
         int sign=1;
         s=s.replaceFirst("^\\s+","");
-        char[] arr= s.toCharArray();
-        if(arr.length==0) return 0;
-        if(arr[0]=='-') sign=-1;
-        for(int i=0;i<arr.length;i++){
+        // char[] arr= s.toCharArray();
+        if(s.length()==0) return 0;
+        if(s.charAt(0)=='-') sign=-1;
+        for(int i=0;i<s.length();i++){
             if(i==0){
-                if(arr[i]=='-'||arr[i]=='+') continue;
+                if(s.charAt(i)=='-'||s.charAt(i)=='+') continue;
             }
-            if(!Character.isDigit(arr[i])) break;
-            num=num*10+ (long) (arr[i]-'0');
+            if(!Character.isDigit(s.charAt(i))) break;
+            num=num*10+ (long) (s.charAt(i)-'0');
             if (sign == 1 && num > Integer.MAX_VALUE) return Integer.MAX_VALUE;
             if (sign == -1 && -num < Integer.MIN_VALUE) return Integer.MIN_VALUE;
         }
-        if(arr[0]=='-') num=num*-1;
+        if(s.charAt(0)=='-') num=num*-1;
         return (int) num;
     }
 }
