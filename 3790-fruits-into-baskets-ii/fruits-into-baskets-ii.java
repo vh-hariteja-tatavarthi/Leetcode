fa18b6436e0970112ab3acd29fc30e531b03b3e5
Tatavarthi Hariteja <tvhhariteja@gmail.com>
2025-08-05 22:04:09 +0530

    Time: 12 ms (5.43%) | Memory: 45.6 MB (5.96%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
@@ -1,14 +1,13 @@
 class Solution {
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        LinkedHashSet<Integer> set= new LinkedHashSet<>();
-        int count = 0;
+HashSet<Integer> set= new HashSet<>();
         for(int i=0;i<fruits.length;i++){
             for(int j=0;j<baskets.length;j++){
                 if(fruits[i]<=baskets[j]){
                     if(set.contains(j)) continue;
 
                     else{
-                        count++;
+
                         set.add(j);
                         break;
 
@@ -17,7 +16,7 @@ class Solution {
             }
         }
         System.out.println(set);
-        return fruits.length-count;
+        return fruits.length-set.size();
         // for(int i: set) System.out.print(i+" ");
         // return fruits.length-set.size();        
     }
