1bb74230a2ae1bcc8f0bba49d9d24d480e705019
Tatavarthi Hariteja <tvhhariteja@gmail.com>
2025-08-07 21:12:10 +0530

    Time: 11 ms (98.51%) | Memory: 45.5 MB (50.87%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -0,0 +1,52 @@
+public class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        if (s.length() == 0 || words.length == 0) return result;
+
+        int wordLen = words[0].length();
+        int wordCount = words.length;
+        int totalLen = wordLen * wordCount;
+
+        // Frequency map of the words
+        HashMap<String, Integer> wordMap = new HashMap<>();
+        for (String word : words) {
+            wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+        }
+
+        // Slide over s in wordLen different offsets to cover all alignment cases
+        for (int i = 0; i < wordLen; i++) {
+            int left = i, right = i, count = 0;
+            HashMap<String, Integer> windowMap = new HashMap<>();
+
+            while (right + wordLen <= s.length()) {
+                String word = s.substring(right, right + wordLen);
+                right += wordLen;
+
+                if (wordMap.containsKey(word)) {
+                    windowMap.put(word, windowMap.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    // If word occurs too many times, shrink window from left
+                    while (windowMap.get(word) > wordMap.get(word)) {
+                        String leftWord = s.substring(left, left + wordLen);
+                        windowMap.put(leftWord, windowMap.get(leftWord) - 1);
+                        left += wordLen;
+                        count--;
+                    }
+
+                    // If all words matched
+                    if (count == wordCount) {
+                        result.add(left);
+                    }
+                } else {
+                    // Reset window
+                    windowMap.clear();
+                    count = 0;
+                    left = right;
+                }
+            }
+        }
+
+        return result;
+    }
+}
