ebee231da840f3293407473ae380b5857a405838
Tatavarthi Hariteja <tvhhariteja@gmail.com>
2025-08-09 16:25:09 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (93.70%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    public boolean isPerfectSquare(int num) {
+
+        if(num < 1)
+        {
+            return false;
+        }
+
+        long low = 1, high = num;
+
+        while(low <= high)
+        {
+            long mid = low + (high - low) / 2;
+
+            long sq = mid * mid;
+
+            if(sq == num)
+            {
+                return true;
+            }
+
+            else if(sq < num)
+            {
+                low = mid + 1;
+            }
+
+            else
+            {
+                high = mid - 1;
+            }
+        }
+
+        return false;
+        
+    }
+}
