af3229f2985222ab1f54c7d03031186dd1cd1590
Tatavarthi Hariteja <tvhhariteja@gmail.com>
2025-08-15 23:04:18 +0530

    Time: 1093 ms (5.85%) | Memory: 0B (100.00%) - LeetSync

--- 1811-fix-names-in-a-table/fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/fix-names-in-a-table.sql
@@ -1,9 +1,4 @@
 # Write your MySQL query statement below
-SELECT 
-    user_id,
-    CONCAT(
-        UPPER(LEFT(name, 1)),                            -- First character, uppercase
-        LOWER(SUBSTRING(name, 2, CHAR_LENGTH(name)))     -- Rest of the string, lowercase
-    ) AS name
+SELECT user_id, CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTRING(name, 2))) as name
 FROM Users
-order by user_id;
+ORDER BY user_id
